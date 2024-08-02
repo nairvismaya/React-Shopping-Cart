fb1dd0b3b7409cb4dd3537591c93dddf9051d4e5
Vismaya Nair <116292754+nairvismaya@users.noreply.github.com>
2024-08-02 17:14:41 +0530

    Initial commit

---
+++ src/components/ShowCourseComponent.js
@@ -0,0 +1,29 @@
+//ShowCourseComponent.js
+import React from "react";
+
+function ShowCourseComponent({
+  courses,
+  filterCourseFunction,
+  addCourseToCartFunction,
+}) {
+  return (
+    <div className="product-list">
+      {filterCourseFunction.length === 0 ? (
+        <p className="no-results">Sorry geek, no matching product!!</p>
+      ) : (
+        filterCourseFunction.map((product) => (
+          <div className="product" key={product.id}>
+            <img src={product.image} alt={product.name} />
+            <h2>{product.name}</h2>
+            <p>Price: ₹{product.price}</p>
+            <button onClick={() => addCourseToCartFunction(product)}>
+              Add to cart
+            </button>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
+
+export default ShowCourseComponent;
